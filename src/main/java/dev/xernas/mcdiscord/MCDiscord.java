f3b3fe4935e2d5cf09812033e6fbc585ba82a7d3
Xernas <Xernas78@gmail.com>
2024-02-18 16:41:11 +0100

    [UPDATE] Added foundation

--- src/main/java/dev/xernas/mcdiscord/MCDiscord.java
+++ src/main/java/dev/xernas/mcdiscord/MCDiscord.java
@@ -1,22 +1,32 @@
 package dev.xernas.mcdiscord;
 
+import dev.xernas.mcdiscord.config.ConfigReader;
+import dev.xernas.mcdiscord.listeners.jda.PrivateMessageSlashListener;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class MCDiscord extends JavaPlugin {
+
+    private static ConfigReader configReader;
 
     @Override
     public void onEnable() {
+        saveDefaultConfig();
+        configReader = new ConfigReader(getConfig());
         Bukkit.getScheduler().runTaskAsynchronously(this, () -> {
-            JDA jda = JDABuilder.createDefault("").build();
+            JDA jda = JDABuilder.createDefault(configReader.getToken()).build();
+            Objects.requireNonNull(jda.getGuildById(configReader.getServerId())).upsertCommand(
+                    Commands.slash("link", "Links your Minecraft account to your discord account")
+                            .addOption(OptionType.STRING, "code", "the code to enter")
+            ).queue();
+            jda.addEventListener(new PrivateMessageSlashListener());
         });
-
     }
 
-    @Override
-    public void onDisable() {
-
-    }
 }
---
+++ src/main/java/dev/xernas/mcdiscord/config/ConfigReader.java
@@ -0,0 +1,38 @@
+package dev.xernas.mcdiscord.config;
+
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.file.FileConfiguration;
+
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ConfigReader {
+
+    private final FileConfiguration config;
+
+    public ConfigReader(FileConfiguration config) {
+        this.config = config;
+    }
+
+    public String getToken() {
+        return config.getString("bot.token");
+    }
+
+    public Long getServerId() {
+        return config.getLong("bot.server-id");
+    }
+
+    public String getGroupFromRole(Long roleId) {
+        ConfigurationSection roles = config.getConfigurationSection("roles");
+        if (roles == null) {
+            return null;
+        }
+        AtomicReference<String> role = new AtomicReference<>("");
+        roles.getValues(false).forEach((string, o) -> {
+            if (o.equals(roleId)) {
+                role.set(string);
+            }
+        });
+        return role.get();
+    }
+
+}
